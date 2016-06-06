4668d775815d5b2085dce998363958e96c6c7f31
hawkleaf <hawkleaf2hotmail.com>
2016-06-06 10:35:00 -0400

    Installed/configured Laravel

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<title> Smoothboard Stylers </title>
+	<!-- Header CSS -->
+	<link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	@yield('header')
+</head>
+<body>
+    @yield('content')
+
+    @yield('footer')
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('header')
+
+@stop
+
+@section('content')
+
+@stop
+
+@section('footer')
+
+@stop
